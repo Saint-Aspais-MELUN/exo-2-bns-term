6a7a9a40da41f6fb0c4680cf3b3e83430c643075
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-02-22 22:10:12 +0100

    Update test_exercice1.py

--- tests/test_exercice1.py
+++ tests/test_exercice1.py
@@ -1,9 +1,9 @@
 from exercices.exercice1 import *
 
-def test_moyenne(lst,n):
+def test_moyenne():
     ## Ici le code d'initialisation si nécessaire
     lst=[2,3,5,9,4]
     assert moyenne([5,3,8])>5,33 and moyenne([5,3,8])<5,34
     assert moyenne([1,2,3,4,5,6,7,8,9,10])==5.5
-        assert moyenne([])=='erreur'
+    assert moyenne([])=='erreur'
     
